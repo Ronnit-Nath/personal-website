99a8f446485fdf5485778b8b6ad2df4add99a459
Ronnit Nath <ronnitnath03@gmail.com>
2024-07-05 20:51:21 +0530

    commit 001

---
+++ app/page.tsx
@@ -0,0 +1,249 @@
+
+import Link from 'next/link';
+import Image from 'next/image';
+import smashing from 'public/images/home/001.jpg';
+import naruto from 'public/images/home/001.jpg';
+import reactathon from 'public/images/home/002.jpg';
+import ship from 'public/images/home/001.jpg';
+import filming from 'public/images/home/001.jpg';
+import meetups from 'public/images/home/001.jpg';
+import vercel from 'public/images/home/001.jpg';
+import avatar from 'app/avatar.jpg';
+
+function Badge(props) {
+  return (
+    <a
+      {...props}
+      target="_blank"
+      className="inline-flex items-center rounded border border-neutral-200 bg-neutral-50 p-1 text-sm leading-4 text-neutral-900 no-underline dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-100"
+    />
+  );
+}
+
+
+
+function ArrowIcon() {
+  return (
+    <svg
+      width="12"
+      height="12"
+      viewBox="0 0 12 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
+
+
+function BlogLink({ slug, name }) {
+  return (
+    <div className="group">
+      <a
+        href={`/blog/${slug}`}
+        className="flex w-full items-center justify-between rounded border border-neutral-200 bg-neutral-50 px-3 py-4 dark:border-neutral-700 dark:bg-neutral-800"
+      >
+        <div className="flex flex-col">
+          <p className="font-medium text-neutral-900 dark:text-neutral-100">
+            {name}
+          </p>
+
+        </div>
+        <div className="transform text-neutral-700 transition-transform duration-300 group-hover:-rotate-12 dark:text-neutral-300">
+          <ArrowIcon />
+        </div>
+      </a>
+    </div>
+  );
+}
+
+export default function Page() {
+  return (
+    <section>
+      <div/>
+      <h1 className="mb-8 text-2xl font-medium tracking-tighter">
+        hey, I'm ronnit 👋
+      </h1>
+      <p className="prose prose-neutral dark:prose-invert">
+        {`lorem ipsum lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum `}
+        <Link href="/work">work</Link>
+        {` lorem ipsumlorem ipsum `}
+        <span className="not-prose">
+
+        </span>
+        {`,lorem ipsumlorem ipsumlorem ipsumlorem ipsum `}
+
+        {` lorem ipsumlorem ipsumlorem ipsumlorem ipsum `}
+        .
+      </p>
+      <div className="grid grid-cols-2 grid-rows-4 sm:grid-rows-3 sm:grid-cols-3 gap-4 my-8">
+        <div className="relative h-40">
+          <Image
+            alt="Me speaking on stage at React Summit about the future of Next.js"
+            src={naruto}
+            fill
+            sizes="(max-width: 768px) 213px, 33vw"
+            priority
+            className="rounded-lg object-cover"
+          />
+        </div>
+        <div className="relative sm:row-span-2 row-span-1">
+          <Image
+            alt="Me standing on stage at Reactathon delivering the keynote"
+            src={reactathon}
+            fill
+            sizes="(max-width: 768px) 213px, 33vw"
+            priority
+            className="rounded-lg object-cover object-top sm:object-center"
+          />
+        </div>
+        <div className="relative">
+          <Image
+            alt="Me and Guillermo Rauch on stage for Vercel Ship, answering questions from the Next.js community"
+            src={ship}
+            fill
+            sizes="(max-width: 768px) 213px, 33vw"
+            priority
+            className="rounded-lg object-cover"
+          />
+        </div>
+        <div className="relative row-span-2">
+          <Image
+            alt="Me, Lydia, and Delba filming the Next.js Conf keynote"
+            src={filming}
+            fill
+            sizes="(max-width: 768px) 213px, 33vw"
+            priority
+            className="rounded-lg object-cover sm:object-center"
+          />
+        </div>
+        <div className="relative row-span-2">
+          <Image
+            alt="My badge on top of a pile of badges from a Vercel meetup we held"
+            src={meetups}
+            fill
+            sizes="(max-width: 768px) 213px, 33vw"
+            priority
+            className="rounded-lg object-cover"
+          />
+        </div>
+        <div className="relative h-40">
+          <Image
+            alt="Me standing on stage at SmashingConf giving a talk about my optimism for the web"
+            src={smashing}
+            fill
+            sizes="(max-width: 768px) 213px, 33vw"
+            priority
+            className="rounded-lg object-cover"
+          />
+        </div>
+      </div>
+      <div className="prose prose-neutral dark:prose-invert">
+        <p>
+        lorem ipsumlorem ipsumlorem ipsum
+        lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum
+        lorem ipsumlorem ipsumlorem ipsum.
+        </p>
+      </div>
+
+      <div className="prose prose-neutral dark:prose-invert">
+        <p>
+        lorem ipsumlorem ipsumlorem ipsum
+        lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum
+        lorem ipsumlorem ipsumlorem ipsum.
+        </p>
+      </div>
+
+
+      
+
+      <div className="my-8 flex w-full flex-col space-y-4">
+        <BlogLink
+          name="Lorem ipsum"
+          slug="2024"
+        />
+        <BlogLink name="What is Developer Relations?" slug="devrel" />
+        <BlogLink name="The Story of Heroku" slug="heroku" />
+      </div>
+      <div className="prose prose-neutral dark:prose-invert">
+        <p>
+        Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
+          Lorem ipsum.
+        </p>
+      </div>
+      <div className="my-8 flex h-14 w-full flex-row space-x-2 overflow-x-auto">
+        <div className="flex items-center justify-between rounded border border-neutral-200 bg-neutral-50 px-3 py-4 dark:border-neutral-700 dark:bg-neutral-800">
+          <a href="https://linear.app">
+            <svg width="78" height="20" role="img" aria-label="Linear logo">
+              <use href="/sprite.svg#linear" />
+            </svg>
+          </a>
+        </div>
+        <div className="flex items-center justify-between rounded border border-neutral-200 bg-neutral-50 px-3 py-4 dark:border-neutral-700 dark:bg-neutral-800">
+          <a href="https://supabase.com">
+            <svg width="100" height="19" role="img" aria-label="Supabase logo">
+              <use href="/sprite.svg#supabase" />
+            </svg>
+          </a>
+        </div>
+        <div className="flex items-center justify-between rounded border border-neutral-200 bg-neutral-50 px-3 py-4 dark:border-neutral-700 dark:bg-neutral-800">
+          <a href="https://www.makeswift.com/blog/makeswift-is-joining-bigcommerce">
+            <svg width="96" height="19" role="img" aria-label="Makeswift logo">
+              <use href="/sprite.svg#makeswift" />
+            </svg>
+          </a>
+        </div>
+        <div className="flex items-center justify-between rounded border border-neutral-200 bg-neutral-50 px-3 py-4 dark:border-neutral-700 dark:bg-neutral-800">
+          <a href="https://resend.com">
+            <svg width="70" height="17" role="img" aria-label="Resend logo">
+              <use href="/sprite.svg#resend" />
+            </svg>
+          </a>
+        </div>
+        <div className="flex items-center justify-between rounded border border-neutral-200 bg-neutral-50 px-3 py-4 dark:border-neutral-700 dark:bg-neutral-800">
+          <a href="https://bun.sh">
+            <svg width="35" height="27" role="img" aria-label="Bun logo">
+              <use href="/sprite.svg#bun" />
+            </svg>
+          </a>
+        </div>
+      </div>
+      <div className="prose prose-neutral dark:prose-invert">
+        <p>
+        Lorem ipsum{' '}
+          <Link href="/blog/2024">Lorem ipsum</Link>,{' '}
+          <Link href="/blog/2024">Lorem ipsum</Link>, Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
+          Lorem ipsumLorem ipsumLorem ipsumLorem ipsum.
+        </p>
+      </div>
+      <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://twitter.com/RonnitNath"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">follow me</p>
+          </a>
+        </li>
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://ronnitnath.substack.com"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">get email updates</p>
+          </a>
+        </li>
+      </ul>
+    </section>
+  );
+}
